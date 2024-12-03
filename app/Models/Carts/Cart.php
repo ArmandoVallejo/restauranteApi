7345164b733129e67ca36d8a606beefa232a8c9f
stonxzz <cuentamlgpro@gmail.com>
2024-12-02 22:10:24 -0600

    Feat: :sparkles: The Api and Crud for Cart-Dishes were created

--- app/Models/Carts/Cart.php
+++ app/Models/Carts/Cart.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Carts;
 
+use App\Models\Dishes\Dish;
 use App\Models\User\User;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,5 +17,12 @@ class Cart extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function dishes()
+    {
+        return $this->belongsToMany(Dish::class, 'cart_dishes', 'cart_id', 'dish_id')
+            ->withPivot(['quantity', 'total_price'])
+            ->withTimestamps();
     }
 }
